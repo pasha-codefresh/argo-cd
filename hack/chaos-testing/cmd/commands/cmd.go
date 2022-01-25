58d145935a8f151a242a1f20c2f5c999be0f6552
pashavictorovich <pavel@codefresh.io>
2022-01-25 12:59:18 +0200

    testing tool
    Signed-off-by: pashavictorovich <pavel@codefresh.io>

---
+++ hack/chaos-testing/cmd/commands/cmd.go
@@ -0,0 +1,46 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+
+	cmdutil "github.com/argoproj/argo-cd/v2/cmd/util"
+	"github.com/argoproj/argo-cd/v2/hack/gen-resources/util"
+	"github.com/argoproj/argo-cd/v2/util/cli"
+)
+
+const (
+	cliName = "argocd-generator"
+)
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	cli.SetLogFormat(cmdutil.LogFormat)
+	cli.SetLogLevel(cmdutil.LogLevel)
+}
+
+// NewCommand returns a new instance of an argocd command
+func NewCommand() *cobra.Command {
+
+	var generateOpts util.GenerateOpts
+
+	var command = &cobra.Command{
+		Use:   cliName,
+		Short: "Generator for argocd resources",
+		Run: func(c *cobra.Command, args []string) {
+			c.HelpFunc()(c, args)
+		},
+		DisableAutoGenTag: true,
+	}
+
+	command.AddCommand(NewGenerateCommand(&generateOpts))
+
+	command.PersistentFlags().StringVar(&generateOpts.Namespace, "kube-namespace", "argocd", "Name of the namespace on which Argo agent should be installed [$KUBE_NAMESPACE]")
+	return command
+}
+
+func NewGenerateCommand(opts *util.GenerateOpts) *cobra.Command {
+	return nil
+}
